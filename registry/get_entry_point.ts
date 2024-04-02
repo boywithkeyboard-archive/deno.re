df2229117e54eeb04f2a3e8cc78943f6cf6b8268
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2024-04-03 01:20:08 +0200

    refactor: remove logging

--- registry/get_entry_point.ts
+++ registry/get_entry_point.ts
@@ -19,7 +19,6 @@ export function getEntryPoint(fileMap: Record<string, string>, path: string): st
   ]
 
   for (const entryPoint of validEntryPoints) {
-    console.log(path + entryPoint)
     if (fileMap[path + entryPoint]) {
       return path + entryPoint
     }
